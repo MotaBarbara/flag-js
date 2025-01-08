5a8d9d09226d2d409c0ef8ab501f56578485dfef
MotaBarbara <barbarantmotaa@gmail.com>
2025-01-08 22:43:29 +0100

    Aula 1

---
+++ js-2.js
@@ -0,0 +1,4 @@
+// "document" is a object
+// get the element with the id "element" from the document
+const el = document.getElementById('element');
+console.log(el); // <div id='element'>Hello World</div>
